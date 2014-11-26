a23145ef142b1b6069f7f8ed9ca95129d3e769d0
dvberkel <daan.v.berkel.1980@gmail.com>
2014-11-26 09:25:16 +0100

    build node in /opt directory

--- scripts/node.sh
+++ scripts/node.sh
@@ -3,12 +3,12 @@
 sudo apt-get -y install g++ curl libssl-dev apache2-utils
 sudo apt-get -y install git-core
 
-mkdir /tmp
-cd /tmp
+mkdir -p /opt
+cd /opt
 
 git clone https://github.com/joyent/node.git
 cd node
-git checkout -b v0.10.33 0.10.33
+git checkout -b 0.10.33 v0.10.33
 ./configure
 make
 sudo make install
